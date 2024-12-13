d2c3f752a5c6a88912652629bca2cdead9aeddc2
Simon Nattrass <simon@nattrass.io>
2024-12-13 14:20:49 +0000

    Clearer man pages

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,4 @@
 export EDITOR='nvim'
 export VISUAL='nvim'
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-#export PAGER='most'
+export MANPAGER="sh -c 'col -bx | bat -l man -p --theme=\"Monokai Extended Bright\"'"
+export PAGER='most'
